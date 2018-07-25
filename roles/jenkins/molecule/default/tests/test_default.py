34795dcb5f72dc9a0f9b727d20686b2e92582ee1
shudarshon <shudarshon@upskillable.com>
2018-07-25 10:33:31 +0000

    added jenkins testcases

--- roles/jenkins/molecule/default/tests/test_default.py
+++ roles/jenkins/molecule/default/tests/test_default.py
@@ -7,4 +7,48 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 def test_java_version(host):
     command = host.run("java -version")
+
     assert command.rc == 0
+
+
+def test_tomcat_user_existence(host):
+    user = host.user('tomcat')
+
+    assert user.exists
+    assert user.shell == '/bin/bash'
+    assert user.home == '/opt/tomcat'
+    assert user.group == 'tomcat'
+
+
+def test_tomcat_home_directory(host):
+    homedir = host.file('/opt/tomcat')
+
+    assert homedir.is_directory
+    assert homedir.user == 'tomcat'
+    assert homedir.group == 'tomcat'
+
+
+def test_tomcat_ssh_directory(host):
+    sshdir = host.file('/opt/tomcat/.ssh')
+
+    assert sshdir.is_directory
+
+
+def test_tomcat_ssh_config(host):
+    sshconf = host.file('/opt/tomcat/.ssh/config')
+
+    assert sshconf.exists
+    assert sshconf.is_file
+
+
+def test_tomcat_port(host):
+    port = host.socket('tcp://0.0.0.0:8080')
+
+    assert port.is_listening
+
+
+def test_tomcat_running_and_enabled(host):
+    tomcat = host.service("tomcat")
+
+    assert tomcat.is_running
+    assert tomcat.is_enabled
