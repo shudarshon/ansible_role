ea3114b8b3def5771cf0d133a2037dad06783388
shudarshon <shudarshon@upskillable.com>
2018-07-26 13:59:35 +0600

    added test case

--- roles/jenkins-slave/molecule/default/tests/test_default.py
+++ roles/jenkins-slave/molecule/default/tests/test_default.py
@@ -8,3 +8,41 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 def test_java_version(host):
     command = host.run("java -version")
     assert command.rc == 0
+
+
+def test_jenkins_user(host):
+    user = host.user('jenkins')
+
+    assert user.exists
+    assert user.shell == '/bin/bash'
+    assert user.home == '/var/lib/jenkins'
+    assert user.group == 'jenkins'
+
+
+def test_jenkins_home_directory(host):
+    homedir = host.file('/var/lib/jenkins')
+
+    assert homedir.is_directory
+    assert homedir.user == 'jenkins'
+    assert homedir.group == 'jenkins'
+
+
+def test_tomcat_ssh_directory(host):
+    sshdir = host.file('/var/lib/jenkins/.ssh')
+
+    assert sshdir.is_directory
+
+
+def test_tomcat_ssh_config(host):
+    sshconf = host.file('/var/lib/jenkins/.ssh/config')
+
+    assert sshconf.exists
+    assert sshconf.is_file
+
+
+def test_tomcat_ssh_auth_key(host):
+    authkey_file = host.file('/var/lib/jenkins/.ssh/authorized_keys')
+
+    assert authkey_file.exists
+    assert authkey_file.is_file
+    assert authkey_file.size > 0
