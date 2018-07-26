2ce24f2a4dab2cfb096a81f4bf32c47b8b1c6973
shudarshon <shudarshon@upskillable.com>
2018-07-26 06:45:06 +0000

    added nginx test case

--- roles/jenkins/molecule/default/tests/test_default.py
+++ roles/jenkins/molecule/default/tests/test_default.py
@@ -11,6 +11,26 @@ def test_java_version(host):
     assert command.rc == 0
 
 
+def test_nginx_user(host):
+    os = host.system_info.distribution
+
+    if os == 'debian':
+        user = host.user('www-data')
+
+        assert user.exists
+        assert user.shell == '/usr/sbin/nologin'
+        assert user.home == '/var/www'
+        assert user.group == 'www-data'
+
+    elif os =='redhat':
+        user = host.user('nginx')
+
+        assert user.exists
+        assert user.shell == '/sbin/nologin'
+        assert user.home == '/var/cache/nginx'
+        assert user.group == 'nginx'
+
+
 def test_tomcat_user_existence(host):
     user = host.user('tomcat')
 
@@ -45,6 +65,19 @@ def test_tomcat_port(host):
     port = host.socket('tcp://0.0.0.0:80')
 
     assert port.is_listening
+
+
+def test_tomcat_port(host):
+    port = host.socket('tcp://0.0.0.0:8080')
+
+    assert port.is_listening
+
+
+def test_nginx_service(host):
+    service = host.service('nginx')
+
+    assert service.is_enabled
+    assert service.is_running
 
 
 def test_tomcat_running_and_enabled(host):
